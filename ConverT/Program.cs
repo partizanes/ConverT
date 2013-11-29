bd5d22da702b9751c7ae8afb6026121eae0daf9d
partizanes <partizanes@bigmir.net>
2013-11-29 11:37:31 +0300

    [4] Easter Egg,any change.

--- ConverT/Program.cs
+++ ConverT/Program.cs
@@ -7,6 +7,7 @@ using System.Data.OleDb;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using ClosedXML.Excel;
+using System.Threading;
 
 namespace ConverT
 {
@@ -21,9 +22,43 @@ namespace ConverT
         uint nSize,
         string lpFileName);
 
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        static IntPtr ConsoleHandle = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+
+        private const int SW_MINIMIZE = 6;
+
+        private const int SW_MAXIMIZE = 3;
+
         static void Main(string[] args)
         {
             WriteLineColor("Приложение запущено.", "Yellow");
+
+            if (args.Length > 0)
+            {
+                int i = 0;
+                while ( i < args.Length )
+                {
+                    WriteLineColor("\n", "Magenta");
+                    WriteLineColor("\n===================================================================", "Magenta");
+                    WriteLineColor("Внимание приложение запущено с аргументом: ", "Yellow");
+                    WriteLineColor(args[i], "Red");
+
+                    if (args[i] == "beep")
+                    {
+                        beep();
+                    }
+
+                    i++;
+                }
+
+                file_processing(args[0],"argument_check");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            ShowWindow(ConsoleHandle, SW_MAXIMIZE);
 
             analysis_directory();
         }
@@ -46,7 +81,8 @@ namespace ConverT
 
                 if (File.Exists(Environment.CurrentDirectory + "//backup//" + EntryDate + "//" + "backup_" + name))
                     File.Delete(Environment.CurrentDirectory + "//backup//" + EntryDate + "//" + "backup_" + name);
-                else
+
+                if (!File.Exists(Environment.CurrentDirectory + "//backup//" + EntryDate + "//" + "backup_" + name))
                     File.Copy(soure + "//" + name, Environment.CurrentDirectory + "//backup//" + EntryDate + "//" + "backup_" + name);
             }
             catch (System.Exception ex)
@@ -108,7 +144,8 @@ namespace ConverT
                                 {
                                     WriteLineColor("\n" + file.Name + " содержит символ очистки количества и сумм бланка", "Yellow");
                                     WriteLineColor("Очистка... ", "Cyan");
-                                    //todo function clean all numbers
+
+                                    clean_num(file.DirectoryName, file.Name);
                                 }
                                 WriteLineColor("Успешно!", "Cyan");
                                 con_true++;
@@ -133,11 +170,155 @@ namespace ConverT
             }
         }
 
-        static Boolean file_processing(string file_patch, string file_name)
+        static Boolean clean_num(string file_patch, string file_name)
         {
             var wb = new XLWorkbook(file_patch + "\\" + file_name);
 
             var ws = wb.Worksheets.Worksheet(1);
+
+            //считаем длину таблицы
+            int total_cell = 16;
+
+            while (ws.Cell(total_cell, 1).GetString() != "")
+            {
+                total_cell++;
+            }
+
+            //очистка всех числовых данных
+            ws.Range("C19:D"+(total_cell-1)).Column(2).Clear();
+
+            ws.Range("F19:G" + (total_cell - 1)).Column(2).Clear();
+
+            //обозначем пунктиром все поля
+            int start_cell = 19;
+
+            while (start_cell < total_cell )
+            {
+                ws.Cell(start_cell, 4).Style.Border.BottomBorder = XLBorderStyleValues.Hair;
+                ws.Cell(start_cell, 4).Style.Border.BottomBorderColor = XLColor.Black;
+
+                ws.Cell(start_cell, 7).Style.Border.LeftBorder = XLBorderStyleValues.Hair;
+                ws.Cell(start_cell, 7).Style.Border.RightBorder = XLBorderStyleValues.Hair;
+                ws.Cell(start_cell, 7).Style.Border.BottomBorder = XLBorderStyleValues.Hair ;
+                ws.Cell(start_cell, 7).Style.Border.BottomBorderColor = XLColor.Black;
+
+                start_cell++;
+            }
+
+            //очищаем линию в конце документа
+            int i =1;
+
+            while (i < 8)
+            {
+                ws.Cell(total_cell-1, i).Style.Border.BottomBorder = XLBorderStyleValues.Hair;
+                ws.Cell(total_cell-1, i).Style.Border.BottomBorderColor = XLColor.Black;
+
+                i++;
+            }
+
+            //очищяем конец документа для добавления доп.строк
+            i = 1;
+
+            while (i <= 9)
+            {
+                ws.Range("A" + (total_cell + i).ToString() + ":I" + (total_cell + i).ToString()).Delete(XLShiftDeletedCells.ShiftCellsLeft);
+                i++;
+            }
+
+            //добавление 10 дополнительных строк для неучтенных сразу строк
+            i = 0;
+
+            while (i <= 10)
+            {
+                int z = 1;
+
+                while (z < 8)
+                {
+                    ws.Cell(total_cell + i, z).Style.Border.LeftBorder = XLBorderStyleValues.Hair;
+                    ws.Cell(total_cell + i, z).Style.Border.RightBorder = XLBorderStyleValues.Hair;
+                    ws.Cell(total_cell + i, z).Style.Border.BottomBorder = XLBorderStyleValues.Hair;
+                    ws.Cell(total_cell + i, z).Style.Border.BottomBorderColor = XLColor.Black;
+
+                    z++;
+                }
+
+                i++;
+            }
+
+
+            //Линия обозначающая конец акта
+            i = 1;
+
+            while (i < 8)
+            {
+                ws.Cell(total_cell + 11, i).Style.Border.TopBorder = XLBorderStyleValues.Medium;
+                ws.Cell(total_cell + 11, i).Style.Border.BottomBorderColor = XLColor.Black;
+
+                i++;
+            }
+
+            int first_insert = total_cell + 12;
+
+            ws.Cell(first_insert, 1).Value = "Всего наименований:";
+            ws.Cell(first_insert , 1).Style.Font.FontName = "Arial";
+            ws.Cell(first_insert , 1).Style.Font.FontSize = 11;
+            ws.Range("A" + first_insert + ":G" + first_insert).Row(1).Merge();
+
+            first_insert = total_cell + 14;
+
+            ws.Cell(first_insert, 1).Value = "Всего единиц товара:";
+            ws.Cell(first_insert, 1).Style.Font.FontName = "Arial";
+            ws.Cell(first_insert, 1).Style.Font.FontSize = 11;
+            ws.Range("A" + first_insert + ":G" + first_insert).Row(1).Merge();
+
+            first_insert = total_cell + 16;
+
+            ws.Cell(first_insert, 1).Value = "На сумму:";
+            ws.Cell(first_insert, 1).Style.Font.FontName = "Arial";
+            ws.Cell(first_insert, 1).Style.Font.FontSize = 11;
+            ws.Range("A" + first_insert + ":G" + first_insert).Row(1).Merge();
+
+            first_insert = total_cell + 18;
+
+            ws.Cell(first_insert, 1).Value = "Материально-ответственное(ые) лицо(а) :";
+            ws.Cell(first_insert, 1).Style.Font.FontName = "Arial";
+            ws.Cell(first_insert, 1).Style.Font.FontSize = 11;
+            ws.Range("A" + first_insert + ":G" + first_insert).Row(1).Merge();
+
+            first_insert = total_cell + 20;
+
+            ws.Cell(first_insert, 1).Value = "Начальник комиссии :";
+            ws.Cell(first_insert, 1).Style.Font.FontName = "Arial";
+            ws.Cell(first_insert, 1).Style.Font.FontSize = 11;
+            ws.Range("A" + first_insert + ":G" + first_insert).Row(1).Merge();
+
+            wb.Save();
+
+            return true;
+        }
+
+        static Boolean file_processing(string file_patch, string file_name)
+        {
+            string par = "\\";
+
+            if (file_name == "argument_check")
+            {
+                par = "";
+                file_name = "";
+            }
+
+            var wb = new XLWorkbook(file_patch + par + file_name);
+
+            var ws = wb.Worksheets.Worksheet(1);
+
+            ws.PageSetup.Margins.Top = 0.208;
+            ws.PageSetup.Margins.Bottom = 0.208;
+            ws.PageSetup.Margins.Left = 0.416;
+            ws.PageSetup.Margins.Right = 0.208;
+            ws.PageSetup.Margins.Footer = 0.333;
+            ws.PageSetup.Margins.Header = 0.333;
+
+            wb.SaveAs(file_patch + par + file_name);
 
             if (ws.Cell(16, 1).Value.ToString() == "Инвентаризационная опись")
             {
@@ -195,7 +376,7 @@ namespace ConverT
 
             ws.Cell(16, 1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
             ws.Cell(16, 1).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
-            ws.Cell(16, 1).Value = "Инвентаризационная опись";
+            ws.Cell(16, 1).Value = "Инвентаризационная опись № " + file_name.ToLower().Replace("c", "").Replace("с", "").Replace("_", "").Replace(".xlsx", "");
 
             ws.Cell(17, 1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
             ws.Cell(17, 1).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
@@ -212,7 +393,7 @@ namespace ConverT
                 {
                     ws.Cell(total_cell, total_coll).Style.Font.FontName = "Arial";
 
-                    ws.Cell(total_cell, total_coll).Style.Font.FontSize = 9;
+                    ws.Cell(total_cell, total_coll).Style.Font.FontSize = 11;
 
                     total_cell++;
                 }
@@ -220,28 +401,40 @@ namespace ConverT
                 total_coll++;
             }
 
+
             int cel1 = total_cell + 1;
             int cel3 = total_cell + 3;
 
             //Добавление в конец файла строк
             ws.Cell(cel1, 1).Value = "Материально-ответственное(ые) лицо(а) :";
             ws.Cell(cel1, 1).Style.Font.FontName = "Arial";
-            ws.Cell(cel1, 1).Style.Font.FontSize = 9;
+            ws.Cell(cel1, 1).Style.Font.FontSize = 11;
             ws.Range("A" + cel1 + ":G"+ cel1).Row(1).Merge();
 
             ws.Cell(cel3, 1).Value = "Начальник комиссии :";
             ws.Cell(cel3, 1).Style.Font.FontName = "Arial";
-            ws.Cell(cel3, 1).Style.Font.FontSize = 9;
+            ws.Cell(cel3, 1).Style.Font.FontSize = 11;
             ws.Range("A" + cel3 + ":G" + cel3).Row(1).Merge();
 
 
             //ширина колонок штрихкод,наименование,сумма.
-            ws.Column(2).Width = 13;
-            ws.Column(3).Width = 52;
-            ws.Column(7).Width = 9;
+            ws.Column(2).Width = 14;
+            ws.Column(3).Width = 70;
+            ws.Column(4).Width = 7;
+            ws.Column(7).Width = 10;
+
+
+//             ws.Column(2).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+//             ws.Column(2).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+//
+//             ws.Column(3).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+//             ws.Column(3).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+//
+//             ws.Column(7).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+//             ws.Column(7).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
 
             //считаем количество страниц на печать
-            int pages = (total_cell+4) / 54;
+            int pages = (total_cell+4) / 35;
 
             //минимальное количество страниц отправляемых на печать.
             if (pages == 0)
@@ -288,6 +481,192 @@ namespace ConverT
             WriteLineColor("Отказ: " + failed, "Red");
             WriteLineColor("\n", "Red");
             WriteLineColor("Понадобиться страниц на печать: " + total_page, "Green");
+        }
+
+        static void beep()
+        {
+            Console.Beep(659, 125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(523, 125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(784, 125);
+            Thread.Sleep(375);
+            Console.Beep(392, 125);
+            Thread.Sleep(375);
+            Console.Beep(523, 125);
+            Thread.Sleep(250);
+            Console.Beep(392, 125);
+            Thread.Sleep(250);
+            Console.Beep(330, 125);
+            Thread.Sleep(250);
+            Console.Beep(440, 125);
+            Thread.Sleep(125);
+            Console.Beep(494, 125);
+            Thread.Sleep(125);
+            Console.Beep(466, 125);
+            Thread.Sleep(42);
+            Console.Beep(440, 125);
+            Thread.Sleep(125);
+            Console.Beep(392, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(784, 125);
+            Thread.Sleep(125);
+            Console.Beep(880, 125);
+            Thread.Sleep(125);
+            Console.Beep(698, 125);
+            Console.Beep(784, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(587, 125);
+            Console.Beep(494, 125);
+            Thread.Sleep(125);
+            Console.Beep(523, 125);
+            Thread.Sleep(250);
+            Console.Beep(392, 125);
+            Thread.Sleep(250);
+            Console.Beep(330, 125);
+            Thread.Sleep(250);
+            Console.Beep(440, 125);
+            Thread.Sleep(125);
+            Console.Beep(494, 125);
+            Thread.Sleep(125);
+            Console.Beep(466, 125);
+            Thread.Sleep(42);
+            Console.Beep(440, 125);
+            Thread.Sleep(125);
+            Console.Beep(392, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(784, 125);
+            Thread.Sleep(125);
+            Console.Beep(880, 125);
+            Thread.Sleep(125);
+            Console.Beep(698, 125);
+            Console.Beep(784, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(587, 125);
+            Console.Beep(494, 125);
+            Thread.Sleep(375);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(415, 125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(698, 125);
+            Thread.Sleep(125);
+            Console.Beep(698, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(625);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(415, 125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(622, 125);
+            Thread.Sleep(250);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(523, 125);
+            Thread.Sleep(1125);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(415, 125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(698, 125);
+            Thread.Sleep(125);
+            Console.Beep(698, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(625);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(415, 125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(622, 125);
+            Thread.Sleep(250);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(523, 125);
+            Thread.Sleep(625);
+
+            WriteLineColor("by part!zanes!", "Cyan");
+
+            beep();
         }
     }
 }
